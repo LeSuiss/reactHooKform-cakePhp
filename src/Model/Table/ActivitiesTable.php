5d79726d533eff76e11c0b3c48c01d36a0472b2a
Alexis ARCHER <archer.alexis@hotmail.fr>
2020-05-21 22:44:57 +0200

    setup back ok, reste completion bdd + css

---
+++ src/Model/Table/ActivitiesTable.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Activities Model
+ *
+ * @method \App\Model\Entity\Activity get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Activity newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Activity[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Activity|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Activity saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Activity patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Activity[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Activity findOrCreate($search, callable $callback = null, $options = [])
+ */
+class ActivitiesTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('activities');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 45)
+            ->allowEmptyString('name');
+
+        $validator
+            ->integer('CO2_per_Unit')
+            ->allowEmptyString('CO2_per_Unit');
+
+        return $validator;
+    }
+}
---
+++ src/Template/Activities/view.ctp
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Activity $activity
+ */
+?>
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Html->link(__('Edit Activity'), ['action' => 'edit', $activity->id]) ?> </li>
+        <li><?= $this->Form->postLink(__('Delete Activity'), ['action' => 'delete', $activity->id], ['confirm' => __('Are you sure you want to delete # {0}?', $activity->id)]) ?> </li>
+        <li><?= $this->Html->link(__('List Activities'), ['action' => 'index']) ?> </li>
+        <li><?= $this->Html->link(__('New Activity'), ['action' => 'add']) ?> </li>
+    </ul>
+</nav>
+<div class="activities view large-9 medium-8 columns content">
+    <h3><?= h($activity->name) ?></h3>
+    <table class="vertical-table">
+        <tr>
+            <th scope="row"><?= __('Name') ?></th>
+            <td><?= h($activity->name) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Id') ?></th>
+            <td><?= $this->Number->format($activity->id) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('CO2 Per Unit') ?></th>
+            <td><?= $this->Number->format($activity->CO2_per_Unit) ?></td>
+        </tr>
+    </table>
+</div>
